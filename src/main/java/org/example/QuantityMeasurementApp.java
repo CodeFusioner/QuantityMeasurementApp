278d9adf4d2fa5c19603e89a34629a977c059aef
Kapil Gupta <kg666235@gmail.com>
2026-03-13 15:29:51 +0530

    [Kapil Gupta] Add. Added UC4

---
+++ src/main/java/org/example/QuantityMeasurementApp.java
@@ -0,0 +1,19 @@
+package main.java.org.example;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+
+        System.out.println("Input: Quantity(1.0, feet) and Quantity(12.0, inches)");
+        System.out.println("Output: Equal (" + q1.equals(q2) + ")");
+
+        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
+        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
+
+        System.out.println("Input: Quantity(1.0, inch) and Quantity(1.0, inch)");
+        System.out.println("Output: Equal (" + q3.equals(q4) + ")");
+    }
+}
