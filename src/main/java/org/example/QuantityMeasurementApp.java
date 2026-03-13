993fb0dabfce442b605990b4f8362edb4440f033
Kapil Gupta <kg666235@gmail.com>
2026-03-13 17:21:13 +0530

    [Kapil Gupta] Add. Added UC13

---
+++ src/main/java/org/example/QuantityMeasurementApp.java
@@ -0,0 +1,27 @@
+package main.java.org.example;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+        Quantity<VolumeUnit> v1 =
+                new Quantity<>(1.0, VolumeUnit.LITRE);
+
+        Quantity<VolumeUnit> v2 =
+                new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+
+        Quantity<VolumeUnit> v3 =
+                new Quantity<>(1.0, VolumeUnit.GALLON);
+
+        System.out.println("Equality:");
+        System.out.println(v1.equals(v2));
+
+        System.out.println("\nConversion:");
+        System.out.println(v1.convertTo(VolumeUnit.MILLILITRE));
+        System.out.println(v3.convertTo(VolumeUnit.LITRE));
+
+        System.out.println("\nAddition:");
+        System.out.println(v1.add(v2));
+        System.out.println(v1.add(v3, VolumeUnit.LITRE));
+    }
+}
